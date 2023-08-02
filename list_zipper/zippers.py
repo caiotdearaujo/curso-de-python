bf88fae2ae05cb37b385836727b324087839e17d
Caio de Araújo <caiotdearaujo@gmail.com>
2023-08-02 17:42:19 -0300

    A fillvalue argument was added

--- list_zipper/zippers.py
+++ list_zipper/zippers.py
@@ -5,15 +5,15 @@ def zip_smallest(list1, list2):
         for i in range(interval)
     ]
 
-def zip_longest(list1, list2):
+def zip_longest(list1, list2, fillvalue=None):
     interval = max(len(list1), len(list2))
     result = []
     for i in range(interval):
         if len(list1) <= i:
-            result.append((None,list2[i]))
+            result.append((fillvalue,list2[i]))
             continue
         if len(list2) <= i:
-            result.append((list1[i],None))
+            result.append((list1[i],fillvalue))
             continue
         result.append((list1[i],list2[i]))
         
